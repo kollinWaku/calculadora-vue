ac66cf1527f8bf746a85a3f760f463d2c3c5218b
kollinWaku <kollinsantos@gmail.com>
2022-06-06 14:21:59 -0300

    ajustes calculadora

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import Dialog from 'primevue/dialog';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+import '/node_modules/primeflex/primeflex.css'
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('Dialog', Dialog);
+
+app.mount('#app')
